ec5a33a6ff605ffe6a71b5aca133a0a5d245dc15
ZiarayZ <jakesmith@binaryrage.com>
2024-03-26 16:15:33 +0000

    create pages - very repetitive html files...

--- server/app/views.py
+++ server/app/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def login(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template("login.html")
+    return HttpResponse(template.render())
+
+
+def signup(request):
+    template = loader.get_template("signup.html")
+    return HttpResponse(template.render())
+
+
+def home(request):
+    template = loader.get_template("home.html")
+    return HttpResponse(template.render())
