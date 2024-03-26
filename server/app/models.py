0488758f30e51381e3791ce97dd95239abdc75a6
ZiarayZ <jakesmith@binaryrage.com>
2024-03-26 17:21:51 +0000

    introduce sql models

--- server/app/models.py
+++ server/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    username = models.CharField(max_length=255, unique=True, primary_key=True)
+    nickname = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+
+class Channels(models.Model):
+    channelid = models.AutoField(unique=True)
+    # message bounds and another table for messages using a foreign key...? How do?
